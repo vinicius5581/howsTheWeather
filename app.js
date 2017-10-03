d989f8fdcc9baca95d0a3111212628a3b788fb1b
Vinicius Santana <santanaviny@gmail.com>
2017-10-02 22:00:05 -0700

    JSON.stringify pretty printing objects

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=300%20Embarcadero%20San%20Francisco`,
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
