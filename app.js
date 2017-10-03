3ef5ed8e2d39e04d23b898b69888f6f9b0679f5a
Vinicius Santana <santanaviny@gmail.com>
2017-10-02 23:01:35 -0700

    yargs + encoded user input

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=300%20Embarcadero%20San%20Francisco`,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`lat: ${body.results[0].geometry.location.lat}`);
+  console.log(`lng: ${body.results[0].geometry.location.lng}`);
 });
